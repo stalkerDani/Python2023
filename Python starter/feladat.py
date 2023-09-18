dc4cd57afb918bbf621b161aa7a1197a1a5dbc92
Muk Dániel <mukd22@ADY-NAGYATAD.SULINET.HU>
2023-09-18 08:45:18 +0200

    gyakorlas

---
+++ Python starter/feladat.py
@@ -0,0 +1,5 @@
+a=5
+if a % 2 ==0:
+    print("páros")
+else:
+    print("páratlan")
---
+++ Python starter/gyakorlás 002.py
@@ -0,0 +1,12 @@
+a=10
+
+a=a+10
+print(a)
+if a<50:
+    a/=2
+    print(a)
+else:
+    a*=10
+print(a)
+
+print (10.5%1)
